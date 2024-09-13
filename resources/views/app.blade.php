9d876db4696598ac42ddb106d7c538b93bedbef8
majweb <marcin.five@gmail.com>
2024-09-13 12:52:58 +0200

    changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Fonts -->
+        <meta property="og:image" content="https://abctest.pl/images/test.png"  />
+        <meta property="og:url" content="https://abctest.pl"  />
+        <meta property="og:type" content="website" />
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <!-- Scripts -->
