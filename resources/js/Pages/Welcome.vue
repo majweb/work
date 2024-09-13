e15dd6a35719762903b05f607da7311feb0b8be1
majweb <marcin.five@gmail.com>
2024-09-13 13:27:45 +0200

    changes

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -9,9 +9,9 @@ import { Head } from '@inertiajs/vue3'
 <template>
     <FrontLayout title="Welcome">
     <Head>
-<!--    <meta property="og:image" content="https://abctest.pl/images/test.jpg"  />-->
-<!--    <meta property="og:url" content="https://abctest.pl"  />-->
-<!--    <meta property="og:type" content="website" />-->
+    <meta property="og:image" content="https://abctest.pl/images/test.jpg"  />
+    <meta property="og:url" content="https://abctest.pl"  />
+    <meta property="og:type" content="website" />
     <title>ss i tata</title>
     <meta name=keywords content="portal pracy, szukam pracy, praca za granicą, porady zdrowotne, przepisy kulinarne, darmowe lekcje niemieckiego" />
     <meta name=description content="Opsi - Work" />
