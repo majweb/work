98bfc51f097d333acb7d78b5eac36adc8a02238c
majweb <marcin.five@gmail.com>
2024-09-13 16:02:40 +0200

    changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
+        <meta inertia property="fb:app_id" content="1234567890" />
+        <meta inertia property="og:type" content="article" />
+        <meta inertia property="og:url" content="https://abctest.pl" />
+        <meta inertia property="og:title" content="Strona główna - work4global" />
+        <meta inertia property="og:image" content="https://abctest.pl/images/test.jpg" />
+        <meta inertia property="og:description" content="pis" />
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
