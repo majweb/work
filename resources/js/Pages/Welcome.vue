ca4200275216971511c2cb01bcd195e8eb04d30e
majweb <marcin.five@gmail.com>
2024-09-13 10:12:53 +0200

    changes

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -8,8 +8,13 @@ import { Head } from '@inertiajs/vue3'
 <template>
     <FrontLayout title="Welcome">
     <Head>
-        <title>mama i tata</title>
-        <meta head-key="description" name="description" content="This is the default description" />
+<title>mama i tata</title>
+<meta name=keywords content="portal pracy, szukam pracy, praca za granicą, porady zdrowotne, przepisy kulinarne, darmowe lekcje niemieckiego" />
+<meta name=description content="Opsi - Work" />
+<meta property="og:url" content="http://64.225.98.176.pl" />
+<meta property="og:title" content="Strona główna" />
+<meta property="og:description" content="facebook" />
+<meta property="og:image" content="https://dobrasztela.pl/storage/pages/strona-glowna-1719495185.png" />
     </Head>
 
     <main class="mt-6 min-h-screen flex items-center justify-center">
