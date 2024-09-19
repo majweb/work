b69b7db38c6d439b97a2997fde670fdde6160d17
majweb <marcin.five@gmail.com>
2024-09-19 16:30:44 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            'media-library-pro-vue3-attachment': '/vendor/spatie/laravel-medialibrary-pro/resources/js/media-library-pro-vue3-attachment',
-            'media-library-pro-vue3-collection': '/vendor/spatie/laravel-medialibrary-pro/resources/js/media-library-pro-vue3-collection',
-        }
-    },
     ssr: {
         noExternal: ['lodash', '@spatie/media-library-pro-vue3-attachment','vue-multiselect','axios'],
     },
