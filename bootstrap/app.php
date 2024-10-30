c52a6eb5911b78b46904325d81f60c20b06b3dab
majweb <marcin.five@gmail.com>
2024-10-30 15:34:06 +0100

    ok

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\Schedule;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -30,5 +31,5 @@ return Application::configure(basePath: dirname(__DIR__))
 //        $schedule->command('media-library:delete-old-temporary-uploads')->daily();
 //    })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        Integration::handles($exceptions);
     })->create();
