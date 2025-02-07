330cae81f4b4531c167272d4dcbf23995a2e4168
majweb <marcin.five@gmail.com>
2025-02-07 12:18:19 +0100

    dzis

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            output:{
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                },
-            },
-        },
-        chunkSizeWarningLimit: 2700,
-    },
     resolve: {
         alias: {
             'media-library-pro-vue3-attachment': '/vendor/spatie/laravel-medialibrary-pro/resources/js/media-library-pro-vue3-attachment',
