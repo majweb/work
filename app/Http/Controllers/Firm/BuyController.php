73bbea8c0998d755cc53e984ac0daaa07ce4b600
majweb <marcin.five@gmail.com>
2025-02-20 12:37:56 +0100

    dzis

--- app/Http/Controllers/Firm/BuyController.php
+++ app/Http/Controllers/Firm/BuyController.php
@@ -113,11 +113,11 @@ class BuyController extends Controller
 
             $sessionId = uniqid() . '-' . time();
             $subtotal = Cart::subtotal();
-Log::info(((float) str_replace(',', '', $subtotal)) .'z zamóienia');
+            Log::info(((float) str_replace(',', '', $subtotal) * 100) .'z zamóienia');
 
             $register = $this->przelewy24->transactions()->register(
                 sessionId: $sessionId,
-                amount: (float) str_replace(',', '', $subtotal),
+                amount: (float) str_replace(',', '', $subtotal) * 100,
                 description: "Kredyty za {$subtotal}",
                 email: request()->user()->email,
                 urlReturn: route('buy.successView'),
