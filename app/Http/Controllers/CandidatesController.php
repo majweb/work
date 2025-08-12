4f44cf6064ca623149457601845bc4325e76c20e
majweb <marcin.five@gmail.com>
2025-08-12 12:52:28 +0200

    main

--- app/Http/Controllers/CandidatesController.php
+++ app/Http/Controllers/CandidatesController.php
@@ -114,7 +114,6 @@ class CandidatesController extends Controller
                 return isset($title[$locale]) && !empty($title[$locale]);
             })
             ->values();
-        dd($categories);
         $customTags = Auth::user()->tags()->orderBy('name')->get();
 
         // Pobieranie tagów przypisanych do kandydata
