73de8d45ec83a75fb06ca2a964af27bdd86f2ec8
majweb <marcin.five@gmail.com>
2026-05-06 07:55:29 +0200

    main

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -291,7 +291,7 @@ onUnmounted(()=>{
                                             <span class="text-xs font-black text-blue-900 leading-none mt-0.5">{{ $page.props.firmLoginPoints || 0 }}</span>
                                         </div>
                                     </div>
-                                    <Link v-if="hasRole('firm') && countCart > 0" :href="route('buy.detail')" class="flex items-center px-3 py-1.5 bg-blue-50 rounded-xl border border-blue-100 shadow-sm group hover:bg-blue-100 transition-colors duration-200 relative">
+                                    <Link v-if="hasRole('firm') && countCart > 0" :href="route('buy.detail')" class="hidden md:flex items-center px-3 py-1.5 bg-blue-50 rounded-xl border border-blue-100 shadow-sm group hover:bg-blue-100 transition-colors duration-200 relative">
                                         <div class="p-1 bg-[#0A2C5C] rounded-lg mr-2 group-hover:scale-110 transition-transform text-white">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-3.5 h-3.5">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
