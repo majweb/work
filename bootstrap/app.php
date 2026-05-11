0a7f424465153fd183552ac7b365b921c3db8cc3
majweb <marcin.five@gmail.com>
2026-05-11 11:01:08 +0200

    main

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -44,7 +44,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         Integration::handles($exceptions);
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
-            if ( !app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+            if ( !app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404])) {
                 return Inertia::render('Error', [
                     'status' => $response->getStatusCode(),
                     'translations' => cache()->rememberForever('translations.'.app()->getLocale(), function () {
