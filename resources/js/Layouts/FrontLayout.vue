57f65189a0def1a22acbe96d35679da4d13fe432
majweb <marcin.five@gmail.com>
2026-05-28 08:21:44 +0200

    main

--- resources/js/Layouts/FrontLayout.vue
+++ resources/js/Layouts/FrontLayout.vue
@@ -282,7 +282,7 @@ const socialLinks = [
             <meta v-if="props.description" property="og:description" :content="props.description" />
             <meta v-if="props.url || ogUrl" property="og:url" :content="props.url || ogUrl" />
             <meta v-if="props.url || ogUrl" property="og:site_name" :content="props.url || ogUrl" />
-            <meta property="og:image" content="https://work4you.global/default-image.png"/>
+            <meta property="og:image" :content="`${props.image || '/default-image.png'}?v=${page.props.assetVersion}`"/>
             <!--        <meta property="og:image" :content="props.image || '/default-image.png'"/>    -->
             <meta property="og:image:width" content="1200" />
             <meta property="og:image:height" content="630" />
